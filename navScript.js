7883a1320685d1afaff20c7ef05ac477532c1159
DorBook <dorb42@gmail.com>
2022-09-20 19:55:22 +0300

    31

--- navScript.js
+++ navScript.js
@@ -6,5 +6,46 @@ const links = document.getElementsByClassName('links')[0]
 toggleMenu.addEventListener("click", () =>{
     buttons.classList.toggle("active")
     links.classList.toggle("active")
-    toggle-submit.classList.toggle("active")
 })
+let form = document.getElementsByClassName("artForm")[0]
+
+form.addEventListener("submit", onSubmit)
+
+function onSubmit(ev){
+    ev.preventDefault()
+    let add = addArt()
+}
+
+function addArt(){
+    let artName = document.getElementById("inputName")
+    let artDesc = document.getElementById("inputDesc")
+    let artLink = document.getElementById("inputLink")
+
+    let divCreate = document.createElement("div")
+    let newName = document.createElement("h1")
+    let newDesc = document.createElement("p")
+    let newImg = document.createElement("img")
+    
+    newName.innerText=artName.value
+    newDesc.innerText=artDesc.value
+    newImg.setAttribute("src",artLink.value)
+
+     
+    divCreate.appendChild(newName)
+    divCreate.appendChild(newImg)
+    divCreate.appendChild(newDesc)
+
+
+    let container =document.getElementsByClassName("container")[0]
+    // let divStyle= getElementsByClassName("art")[0]
+    // divStyle.appendChild(divCreate)
+    container.appendChild(divCreate)
+
+    
+    let add = artName.value
+    artName=""
+    artDesc=""
+    return add
+}
+
+
